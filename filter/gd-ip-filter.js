d13ddaffc499a153b87e59b03665dc4f3fffd2b4
tanzl <16420435+tanzl@users.noreply.github.com>
2022-09-05 16:29:22 +0800

    🚧 简化逻辑

--- filter/gd-ip-filter.js
+++ filter/gd-ip-filter.js
@@ -1,11 +1,5 @@
 function filter(proxies) {
-  return proxies.map(p => {
-    const { type } = p
-
-    /* 只修改 vmess 和 vless */
-    if (['vmess', 'vless'].includes(type)) {
-      return /(?<![.\d])\d{3}/.test(p.server)
-    }
-    return true
+  return proxies.map(proxy => {
+    return new RegExp('(?<![.\\d])\\d{3}').test(proxy.server);
   });
 }
